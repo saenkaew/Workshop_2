dbfd14bf16ba55c52b2f434d9300765dbf60c87e
saenkaew <saenkaew.s@ku.th>
2021-01-16 13:31:55 +0700

    Add slicing.py

---
+++ String/slicing.py
@@ -0,0 +1,8 @@
+#Slicing
+string = "Hello World"
+print(string[2:5]) # llo
+print(string[:5]) # Hello
+print(string[2:]) # llo World
+print(string[-5:-2]) # Wor
+print(string[0]) # H
+print(string[-1:]) # d
